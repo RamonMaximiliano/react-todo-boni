6653f1a8a0b42f13e3df16bc7cb67ce9bff72ff6
Ramon Maximiliano Wilbert <ramon_max1986@hotmail.com>
2022-05-16 23:31:05 -0300

    continuing task component

--- src/component/TaskInput.js
+++ src/component/TaskInput.js
@@ -1,6 +1,27 @@
-function TaskInput(){
+import { useState } from "react"
+
+function TaskInput() {
+    const [task, setTask] = useState('')
+
+    const submitted = (e) => {
+        e.preventDefault()
+
+        console.log(e)
+        console.log(task)
+
+
+/*         quando der o submitted, jogar a task finalizada para a lista de tasks
+        limpar o state para poder escrever a nova task
+ */
+
+    }
+
     return (
-        <input placeHolder="+ Adicione uma tarefa" className="taskinput"></input>
+        /* o onSubmit abaixo esta trigando a function submitted, que vai submeter a task finalizada e setar de novo o state para zero */
+        <form onSubmit={submitted}>
+            {/*  o onChange do input abaixo esta setando o state da task como o que estiver sendo escrito  */}
+            <input type="text" placeHolder="+ Adicione uma tarefa" className="taskinput" onChange={e => setTask(e.target.value)}></input>
+        </form>
     );
 }
 
