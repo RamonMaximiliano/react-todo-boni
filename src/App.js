d4bdae55a3f79a0bf16e83919628684ec4c3d9a2
Ramon <ramon_max1986@hotmail.com>
2022-05-19 12:58:35 -0300

    styled checked

--- src/App.js
+++ src/App.js
@@ -5,34 +5,35 @@ import Footer from './component/Footer'
 import Tasks from './component/Tasks'
 import Delete from './component/Delete'
 import { useState } from "react"
+import { toHaveAccessibleName } from '@testing-library/jest-dom/dist/matchers';
 
 function App() {
 
   //criou um state para ser atualizado através do método taskInfoMethod
   const [taskInfo, taskInfoMethod] = useState('');
   const [taskList, taskListupdate] = useState([
-     {
+    {
       id: 0,
       name: 'Feed dog'
     },
     {
       id: 1,
       name: 'Buy food'
-    }, 
-    
+    },
+
     {
       id: 2,
       name: 'Buy food'
-    } 
+    }
   ]);
 
   const submitted = (e) => {
     e.preventDefault()
 
     let identification = taskList.length
-    let newTask = '' 
+    let newTask = ''
 
-    if (taskInfo == ''){
+    if (taskInfo == '') {
       window.alert('Type something!')
     } else {
 
@@ -40,39 +41,52 @@ function App() {
         id: identification + 1,
         name: taskInfo
       }
-       taskListupdate([...taskList, newTask]);
-     }
-     console.log(taskList)
-     document.getElementById("inputText").value = ''
-     taskInfoMethod('')
- }
+      taskListupdate([...taskList, newTask]);
+    }
+    console.log(taskList)
+    document.getElementById("inputText").value = ''
+    taskInfoMethod('')
+  }
 
 
-const cleanList  = function cleanList(){
-  taskListupdate([])
-}
+  const cleanList = function cleanList() {
+    taskListupdate([])
+  }
 
-const check = function checkItem(checked){
-  console.log(checked)
+  const check = function checkItem(checked) {
+    const checkedItem = taskList.filter(function (item) {
+      return item.id == checked
+    })
 
-/* 
-  itemResult.addEventListener("click", function checked(event) {
-    event.target.style.textDecoration = "line-through";
-}) */
+    let selector = checkedItem[0].id.toString()
+    let itemChecked = document.querySelector(`p.number${selector}`)
+    let itemSelected = document.getElementById(`${selector}`)
 
-}
+    /* - Selecionando o elemento conforme ID e verificando se o input check, esta checkado ou não pois é boleano e pode ser usado em clausula IF, decorando texto de acordo com resultado
+    - No selecionados "itemChecked" acima, tive que por o texto "number" na class pois o CSS3 não deixa por numeros como classes, elas tem que começar com algum texto 
+     */
+
+    if (itemSelected.checked) {
+      itemChecked.style.textDecoration = "line-through"
+      document.getElementById(`name${selector}`).classList.add("checkedColor")
+
+    } else {
+      itemChecked.style.textDecoration = "none"
+      document.getElementById(`name${selector}`).classList.remove("checkedColor")
+    }
+  }
 
 
   return (
     <div className="mainApp">
       <Header />
       {/*       a prop abaixo esta recebendo o método que atualiza o state taskInfo
- */}      <TaskInput typed={taskInfoMethod} onsubmitText={submitted} key='5'/>
+ */}      <TaskInput typed={taskInfoMethod} onsubmitText={submitted} key='5' />
 
       {
         taskList.map(item => <Tasks name={item.name} id={item.id} checked={check} />)
       }
-      <Delete delete={cleanList}/>
+      <Delete delete={cleanList} />
       <Footer />
     </div>
   );
@@ -89,20 +103,10 @@ export default App;
 -- Add item
 -- Show list of items
 Check bug continue adding even without text on input box
-Click finished item 
+-- Click finished item 
 Delete item 
 -- Clear list
 -- Add footer 
-
-
-
-cada input já tem um ID, 
-
-
-
-
-
-
 
 */
 
--- src/component/Tasks.js
+++ src/component/Tasks.js
@@ -2,12 +2,12 @@ import { useState } from "react"
 
 function Tasks(props) {
     return (
-        <div className="singletask" >
-{/*             passando o props id para poder excluir este item específico depois na função check
+        <div className="singletask" id={`name${props.id}`}>
+            {/*             passando o props id para poder excluir este item específico depois na função check
  */}            <input type="checkbox" id={props.id} onClick={() => props.checked(props.id)}>
             </input>
             <div className="paragraph">
-                <p>{props.name}</p>
+                <p className={`number${props.id}`}>{props.name}</p>
                 <p>{props.id}</p>
             </div>
         </div>
